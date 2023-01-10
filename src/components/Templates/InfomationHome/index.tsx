b2394ad7d5bf0c16ec8534b4c5ce705303d23d68
4zur <AzureL4zuli@gmail.com>
2023-01-10 13:22:30 +0900

    save

---
+++ src/components/Templates/InfomationHome/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next'
+import PageInfomationHomePresenter from './presenter'
+
+const InfomationHomeContainer: NextPage = () => {
+  return <PageInfomationHomePresenter />
+}
+
+export default InfomationHomeContainer
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,52 +1,10 @@
-import { AppProps } from 'next/app'
-import Head from 'next/head'
-import { createGlobalStyle } from 'styled-components'
+import { ChakraProvider } from '@chakra-ui/react'
+import type { AppProps } from 'next/app'
 
-// グローバル
-
-const GlobalStyle = createGlobalStyle`
-html,
-body,
-textarea {
-  padding: 0;
-  margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Robotom Oxygen,
-    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-  }
-  
-  * {
-    box-sizing: border-box;
-  }
-
-  a {
-    cursor: pointer;
-    text-decoration: none;
-    transition: .25s;
-    color: #000;
-  }
-
-  ol, ul {
-    list-style: none;
-  }
-}`
-
-const MyApp = ({ Component, pageProps }: AppProps) => {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head>
-        <meta key="charset" name="charset" content="utf-8" />
-        <meta
-          key="viewport"
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
-        />
-        <meta property="og:local" content="ja_JP" />
-        <meta property="og:type" content="website" />
-      </Head>
-      <GlobalStyle />
+    <ChakraProvider>
       <Component {...pageProps} />
-    </>
+    </ChakraProvider>
   )
 }
-
-export default MyApp
